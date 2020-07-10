31243770fcc89b31cc764ec6132f51b74f538b14
Javier Benavides <42157462+chatojavier@users.noreply.github.com>
2020-07-10 00:43:01 -0500

    empty wp _s theme

---
+++ themes/qatatheme/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var plumber = require('gulp-plumber');
+
+function onError(err) {
+    console.log(err);
+}
+
+gulp.task('sass', function(){
+    return gulp.src('assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(minify())
+        .pipe(gulp.dest('./assets/css/'))
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+});
+
+//Watch task
+gulp.task('default', function() {
+    gulp.watch('assets/sass/**/*.scss', gulp.series('sass'));
+});
