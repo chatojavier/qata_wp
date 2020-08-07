559d448a0d6eb66c8987de061eec0603e8464a19
Javier Benavides <42157462+chatojavier@users.noreply.github.com>
2020-08-06 21:18:31 -0500

    Show and hide sticky bar

--- themes/qatatheme/assets/js/main.js
+++ themes/qatatheme/assets/js/main.js
@@ -47,7 +47,6 @@
 // When the user scrolls down 100px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
-
 function scrollFunction() {
     
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -58,6 +57,18 @@ function scrollFunction() {
         document.querySelector("img.logo").removeAttribute("style");
         document.querySelector(".fixed-bar").removeAttribute("style");
         document.querySelector(".menu-btn").removeAttribute("style");
+    }
+    if (document.querySelectorAll(".item").length < 1) {
+        if (document.body.scrollTop > (window.innerHeight - 200) || document.documentElement.scrollTop > (window.innerHeight - 200)) {
+            document.querySelector("header.header").style.display = "block";
+        } else {
+            document.querySelector("header.header").removeAttribute("style");
+        }
+        if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+            document.querySelector("header.header").style.opacity = "1";
+        } else {
+            document.querySelector("header.header").style.opacity = "0";
+        }
     }
 }
 
